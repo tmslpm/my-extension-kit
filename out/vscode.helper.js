e93a983eb3253a950f3a0349283afb2f7a54acf2
tmslpm <thomaslepomme@gmail.com>
2024-05-17 04:14:45 +0200

    save before test cleanup

--- out/vscode.helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hasExtension = void 0;
-const vscode_1 = require("vscode");
-function hasExtension(id) {
-    return vscode_1.extensions.getExtension(id) !== undefined;
-}
-exports.hasExtension = hasExtension;
-//# sourceMappingURL=vscode.helper.js.map
